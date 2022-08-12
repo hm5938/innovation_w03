1034bbc79ea3579170db7c43dfd6c99561c23341
Hyemm <hmlee5938@gmail.com>
2022-08-12 16:57:02 +0900

    환경설정

---
+++ post_CRUD/src/main/java/com/sparta/post_crud/PostCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.post_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostCrudApplication.class, args);
+    }
+
+}
---
+++ post_CRUD/src/test/java/com/sparta/post_crud/PostCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.post_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
