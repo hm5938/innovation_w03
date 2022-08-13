2588e332b4bca8e674a820deaf3db2b8824260b6
Hyemm <hmlee5938@gmail.com>
2022-08-13 21:56:28 +0900

    Update PostService.java

--- post_CRUD/src/main/java/com/sparta/post_crud/service/PostService.java
+++ post_CRUD/src/main/java/com/sparta/post_crud/service/PostService.java
@@ -4,7 +4,11 @@ import com.sparta.post_crud.dto.PostRequestDto;
 import com.sparta.post_crud.entity.Post;
 import com.sparta.post_crud.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -17,4 +21,31 @@ public class PostService {
         post1.update(postRequestDto);
         return post1.getId();
     }
+
+    public Post read(Long id){
+        Post post1= postRepository.findById(id).orElseThrow(()-> new IllegalArgumentException());
+        return post1;
+    }
+
+    public boolean checkPassword(Long id, String password){
+        Post post1 = postRepository.findById(id).orElseThrow(()-> new IllegalArgumentException());
+        return post1.getPassword().equals(password);
+    }
+
+    public Long delete(Long id){
+        postRepository.deleteById(id);
+        return id;
+    }
+
+    public Post createPost(PostRequestDto postRequestDto){
+        Post post = new Post(postRequestDto);
+        return postRepository.save(post);
+    }
+
+
+    public List<Post> getList(){
+        List<Post> posts= postRepository.findAll(Sort.by("createdAt"));
+        return posts;
+    }
+
 }
