c5193e23fd4abead26f63ea3e460b4f0df3f6384
Hyemm <hmlee5938@gmail.com>
2022-08-17 04:17:12 +0900

    Create PostResponseDto.java

---
+++ post_CRUD/src/main/java/com/sparta/post_crud/dto/PostResponseDto.java
@@ -0,0 +1,26 @@
+package com.sparta.post_crud.dto;
+
+import com.sparta.post_crud.entity.Post;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class PostResponseDto {
+    private String title;
+    private String name;
+   private Long id;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public PostResponseDto(Post post){
+        this.id = post.getId();
+        this.title= post.getTitle();
+        this.name = post.getName();
+        this.createdAt = post.getCreatedAt();
+        this.modifiedAt = post.getModifiedAt();
+    }
+}
