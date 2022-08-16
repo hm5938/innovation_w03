d53281f8e4b00fddc0707ed50daffa451e8f09e9
Hyemm <hmlee5938@gmail.com>
2022-08-17 04:17:04 +0900

    데이터생성시간 저장을 위한 클래스 생성

--- post_CRUD/src/main/java/com/sparta/post_crud/PostCrudApplication.java
+++ post_CRUD/src/main/java/com/sparta/post_crud/PostCrudApplication.java
@@ -1,13 +1,26 @@
 package com.sparta.post_crud;
 
+import com.sparta.post_crud.dto.PostRequestDto;
+import com.sparta.post_crud.entity.Post;
+import com.sparta.post_crud.repository.PostRepository;
+import com.sparta.post_crud.service.PostService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.List;
+@EnableJpaAuditing
 @SpringBootApplication
 public class PostCrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PostCrudApplication.class, args);
     }
-
+    @Bean
+    public CommandLineRunner demo(PostRepository postRepository, PostService postService) {
+        return (args) -> {
+        };
+    }
 }
--- post_CRUD/src/main/java/com/sparta/post_crud/entity/Timestamped.java
+++ post_CRUD/src/main/java/com/sparta/post_crud/entity/Timestamped.java
@@ -1,5 +1,6 @@
 package com.sparta.post_crud.entity;
 
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -8,7 +9,8 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@MappedSuperclass // 상속했을 때, 컬럼으로 인식하게 합니다.
+@MappedSuperclass
+@Getter// 상속했을 때, 컬럼으로 인식하게 합니다.
 @EntityListeners(AuditingEntityListener.class) // 생성/수정 시간을 자동으로 반영하도록 설정
 public class Timestamped {
 
