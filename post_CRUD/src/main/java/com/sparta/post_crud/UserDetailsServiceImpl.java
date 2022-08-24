f74160ef69c86eb3eacd0c33fd603562e74baaf3
Hyemm <hmlee5938@gmail.com>
2022-08-25 06:05:27 +0900

    user 객체 생성

---
+++ post_CRUD/src/main/java/com/sparta/post_crud/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.sparta.post_crud;
+
+import com.sparta.post_crud.entity.User;
+
+import com.sparta.post_crud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        List<User> user1 = userRepository.findAll();
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Can't find " + username));
+
+        return new UserDetailsImpl(user);
+    }
+}
