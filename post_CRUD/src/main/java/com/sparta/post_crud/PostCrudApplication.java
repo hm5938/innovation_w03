304e1c57870c17a6c938c295417f676a29a34370
Hyemm <hmlee5938@gmail.com>
2022-08-25 06:07:05 +0900

    Update PostCrudApplication.java

--- post_CRUD/src/main/java/com/sparta/post_crud/PostCrudApplication.java
+++ post_CRUD/src/main/java/com/sparta/post_crud/PostCrudApplication.java
@@ -1,17 +1,13 @@
 package com.sparta.post_crud;
 
-import com.sparta.post_crud.dto.PostRequestDto;
-import com.sparta.post_crud.entity.Post;
-import com.sparta.post_crud.repository.PostRepository;
-import com.sparta.post_crud.service.PostService;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.List;
 @EnableJpaAuditing
+@ServletComponentScan("lecturer")
 @SpringBootApplication
 public class PostCrudApplication {
 
